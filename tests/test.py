1e0c3983b807d0b151e879605e233e5710ec5e5a
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2021-01-20 21:37:22 -0300

    Fix small test bug

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,8 @@ def test_url_parser():
     thread_id = "214860910"
     thread_board = "a"
     thread_url_4chan = "https://boards.4channel.org/a/thread/{}/boku-no-hero-academia"
-    thread = FourChanAPIE.parse_thread_url(thread_url_4chan.format(thread_id))
+    thread_url_4chan = thread_url_4chan.format(thread_id)
+    thread = FourChanAPIE.parse_thread_url(thread_url_4chan)
     assert thread_id == thread.tid
     assert thread_board == thread.board
     assert thread_url_4chan == thread.url
